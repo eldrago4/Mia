4443e7bf688d46852da9d0623a12f92bd8d3056c
eldrago <63483703+eldrago4@users.noreply.github.com>
2021-07-14 10:25:34 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-"""
-Flask Config File
-Gets sensitive client (us) auth codes from file
-"""
-with open("g_auth.txt") as f:
-    f.__iter__()
-    GOOGLE_CLIENT_ID = f.__next__().strip("\n")
-    GOOGLE_CLIENT_SECRET = f.__next__().strip("\n\t")
